7702b9a3d684efc6bea77ab2659e9ef33c3e1bfa
Peter Yuan <Peter.Yuan@Tm-robot.com>
2022-09-21 17:57:32 +0800

    加入專案檔案。

---
+++ Service/MemBuffer.cs
@@ -0,0 +1,18 @@
+﻿namespace _0921Demo.Service
+{
+    public class MemBuffer
+    {
+        private int currentRound = 0;
+        public int GetCurrentRound()
+        {
+            return currentRound;
+        }
+        public void SetCurrentRound(int a)
+        {
+            currentRound = a;
+            CurrentRoundChange(a);
+        }
+        public event Action<int>? OnCurrentRoundChange;
+        public void CurrentRoundChange(int currentRound) => OnCurrentRoundChange?.Invoke(currentRound);
+    }
+}
